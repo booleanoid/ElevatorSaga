d95a52331a350df8cb960376c6c476d4f279d375
booleanoid <booleanoid@gmail.com>
2021-07-15 17:07:55 +0900

    add floor button pressed, floor button pressed

--- ElevatorSaga.js
+++ ElevatorSaga.js
@@ -1,15 +1,25 @@
 {
-    init: function(elevators, floors) {
-        var elevator = elevators[0]; // Let's use the first elevator
+  init: function(elevators, floors) {
+    let e = elevators[0]; // Let's use the first elevator
 
-        // Whenever the elevator is idle (has no more queued destinations) ...
-        elevator.on("idle", function() {
-            // let's go to all the floors (or did we forget one?)
-            elevator.goToFloor(0);
-            elevator.goToFloor(1);
-        });
-    },
-    update: function(dt, elevators, floors) {
-        // We normally don't need to do anything here
-    }
+    // Whenever the elevator is idle (has no more queued destinations) ...
+    e.on("idle", function() {
+      // let's go to all the floors (or did we forget one?)
+      e.goToFloor(0);
+      // e.goToFloor(1);
+    });
+    e.on("floor_button_pressed", function(fn){
+      e.goToFloor(fn);
+    });
+    floors.forEach(function(f){
+      f.on("up_button_pressed down_button_pressed", function(){
+        e.goToFloor(f.floorNum())
+      })
+
+    })
+
+  },
+  update: function(dt, elevators, floors) {
+    // We normally don't need to do anything here
+  }
 }
